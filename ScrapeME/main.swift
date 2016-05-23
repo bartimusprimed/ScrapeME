c0be4a3de561c468674860252e3d29b756aab874
Samuel Barthelemy <samuelbarthelemy@gmail.com>
2016-05-23 18:48:17 -0400

    Initial Commit

---
+++ ScrapeME/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ScrapeME
+//
+//  Created by Samuel Barthelemy on 5/23/16.
+//  Copyright © 2016 Samuel Barthelemy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
